6ba3196ce15a4756ca4b02e68b7e0f51698f3f0f
WooSungD <woosung@float.capital>
2023-10-23 12:08:49 +0100

    creation of pools from factory and updating of reserves of single pool

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -5,14 +5,42 @@
 // code below is purely for demonstration purposes
 // please use it for reference only and delete it when you start working on your indexer
 
-import { PoolContract_Sync_handler } from "../generated/src/Handlers.gen";
+import {
+  FactoryContract_PoolCreated_loader,
+  FactoryContract_PoolCreated_handler,
+  PoolContract_Sync_loader,
+  PoolContract_Sync_handler,
+} from "../generated/src/Handlers.gen";
+import { poolEntity } from "./src/Types.gen";
+
+FactoryContract_PoolCreated_handler(({ event, context }) => {
+  let pool_instance: poolEntity = {
+    id: event.params.pool.toString(),
+    token0: event.params.token0.toString(),
+    token1: event.params.token1.toString(),
+    stable: event.params.stable,
+    reserve0: BigInt(0),
+    reserve1: BigInt(0),
+  };
+
+  console.log("pool_instance", pool_instance);
+
+  context.pool.set(pool_instance);
+});
+
+PoolContract_Sync_loader(({ event, context }) => {
+  let _ = context.pool.load(event.srcAddress.toString());
+});
 
 PoolContract_Sync_handler(({ event, context }) => {
-  const pair_instance = {
-    id: String(event.blockNumber),
-    timestamp: BigInt(event.blockTimestamp),
-    reserve0: BigInt(event.params.reserve0),
-    reserve1: BigInt(event.params.reserve1),
-  };
-  context.pair.set(pair_instance);
+  let current_pool = context.pool.get(event.srcAddress.toString());
+
+  if (current_pool) {
+    const pool_instance: poolEntity = {
+      ...current_pool,
+      reserve0: BigInt(event.params.reserve0),
+      reserve1: BigInt(event.params.reserve1),
+    };
+    context.pool.set(pool_instance);
+  }
 });
