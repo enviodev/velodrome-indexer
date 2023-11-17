a39c6b71a3de7a251f36e0339816b181a146eb5f
WooSungD <woosung@float.capital>
2023-11-17 15:00:36 +0000

    working version

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,32 +1,8 @@
-/*
- *Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features*
- */
-
-// code below is purely for demonstration purposes
-// please use it for reference only and delete it when you start working on your indexer
-
 import {
-  FactoryContract_PoolCreated_loader,
-  FactoryContract_PoolCreated_handler,
   PoolContract_Sync_loader,
   PoolContract_Sync_handler,
 } from "../generated/src/Handlers.gen";
 import { poolEntity } from "./src/Types.gen";
-
-FactoryContract_PoolCreated_handler(({ event, context }) => {
-  let pool_instance: poolEntity = {
-    id: event.params.pool.toString(),
-    token0: event.params.token0.toString(),
-    token1: event.params.token1.toString(),
-    stable: event.params.stable,
-    reserve0: BigInt(0),
-    reserve1: BigInt(0),
-  };
-
-  console.log("pool_instance", pool_instance);
-
-  context.Pool.set(pool_instance);
-});
 
 PoolContract_Sync_loader(({ event, context }) => {
   let _ = context.Pool.load(event.srcAddress.toString());
@@ -40,6 +16,15 @@ PoolContract_Sync_handler(({ event, context }) => {
       ...current_pool,
       reserve0: BigInt(event.params.reserve0),
       reserve1: BigInt(event.params.reserve1),
+    };
+    context.Pool.set(pool_instance);
+  } else {
+    const pool_instance: poolEntity = {
+      id: event.srcAddress.toString(),
+      reserve0: BigInt(event.params.reserve0),
+      reserve1: BigInt(event.params.reserve1),
+      cumulativeVolume0: BigInt(0),
+      cumulativeVolume1: BigInt(0),
     };
     context.Pool.set(pool_instance);
   }
