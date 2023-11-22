a353ae5c71cf58b1cb6ed5334fcd5a3d5e7d52ec
WooSungD <woosung@float.capital>
2023-11-22 10:57:22 +0000

    indexing stablecoin pools and calculation of relative token price in pool

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -8,6 +8,8 @@ import {
 } from "../generated/src/Handlers.gen";
 import { poolEntity } from "./src/Types.gen";
 
+const TEN_TO_THE_18_BI = BigInt(10 ^ 18);
+
 PoolContract_Fees_loader(({ event, context }) => {
   let _ = context.Pool.load(event.srcAddress.toString());
 });
@@ -27,13 +29,15 @@ PoolContract_Fees_handler(({ event, context }) => {
   } else {
     const pool_instance: poolEntity = {
       id: event.srcAddress.toString(),
-      reserve0: BigInt(0),
-      reserve1: BigInt(0),
-      cumulativeVolume0: BigInt(0),
-      cumulativeVolume1: BigInt(0),
+      reserve0: 0n,
+      reserve1: 0n,
+      cumulativeVolume0: 0n,
+      cumulativeVolume1: 0n,
       cumulativeFees0: BigInt(event.params.amount0),
       cumulativeFees1: BigInt(event.params.amount1),
-      numberOfSwaps: BigInt(1),
+      numberOfSwaps: 1n,
+      token0Price: 0n,
+      token1Price: 0n,
     };
     context.Pool.set(pool_instance);
   }
@@ -53,19 +57,21 @@ PoolContract_Swap_handler(({ event, context }) => {
         current_pool.cumulativeVolume0 + BigInt(event.params.amount0In),
       cumulativeVolume1:
         current_pool.cumulativeVolume1 + BigInt(event.params.amount1In),
-      numberOfSwaps: current_pool.numberOfSwaps + BigInt(1),
+      numberOfSwaps: current_pool.numberOfSwaps + 1n,
     };
     context.Pool.set(pool_instance);
   } else {
     const pool_instance: poolEntity = {
       id: event.srcAddress.toString(),
-      reserve0: BigInt(0),
-      reserve1: BigInt(0),
+      reserve0: 0n,
+      reserve1: 0n,
       cumulativeVolume0: BigInt(event.params.amount0In),
       cumulativeVolume1: BigInt(event.params.amount1In),
-      cumulativeFees0: BigInt(0),
-      cumulativeFees1: BigInt(0),
-      numberOfSwaps: BigInt(1),
+      cumulativeFees0: 0n,
+      cumulativeFees1: 0n,
+      numberOfSwaps: 1n,
+      token0Price: 0n,
+      token1Price: 0n,
     };
     context.Pool.set(pool_instance);
   }
@@ -79,22 +85,47 @@ PoolContract_Sync_handler(({ event, context }) => {
   let current_pool = context.Pool.get(event.srcAddress.toString());
 
   if (current_pool) {
+    let token0Price = current_pool.token0Price;
+    let token1Price = current_pool.token1Price;
+
+    if (event.params.reserve0 != 0n && event.params.reserve1 != 0n) {
+      token1Price =
+        (TEN_TO_THE_18_BI * event.params.reserve1) / event.params.reserve0;
+
+      token0Price =
+        (TEN_TO_THE_18_BI * event.params.reserve0) / event.params.reserve1;
+    }
+
     const pool_instance: poolEntity = {
       ...current_pool,
       reserve0: BigInt(event.params.reserve0),
       reserve1: BigInt(event.params.reserve1),
+      token0Price,
+      token1Price,
     };
     context.Pool.set(pool_instance);
   } else {
+    let token0Price = 0n;
+    let token1Price = 0n;
+    if (event.params.reserve0 != 0n && event.params.reserve1 != 0n) {
+      token1Price =
+        (TEN_TO_THE_18_BI * event.params.reserve1) / event.params.reserve0;
+
+      token0Price =
+        (TEN_TO_THE_18_BI * event.params.reserve0) / event.params.reserve1;
+    }
+
     const pool_instance: poolEntity = {
       id: event.srcAddress.toString(),
       reserve0: BigInt(event.params.reserve0),
       reserve1: BigInt(event.params.reserve1),
-      cumulativeVolume0: BigInt(0),
-      cumulativeVolume1: BigInt(0),
-      cumulativeFees0: BigInt(0),
-      cumulativeFees1: BigInt(0),
-      numberOfSwaps: BigInt(0),
+      cumulativeVolume0: 0n,
+      cumulativeVolume1: 0n,
+      cumulativeFees0: 0n,
+      cumulativeFees1: 0n,
+      numberOfSwaps: 0n,
+      token0Price,
+      token1Price,
     };
     context.Pool.set(pool_instance);
   }
