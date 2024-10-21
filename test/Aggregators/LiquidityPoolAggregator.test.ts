6439fd526d050084b3d1c0108db47e7fb6dc0a1a
jrab227@gmail.com <jrab227@gmail.com>
2024-10-21 12:45:50 -0400

    Update tests.

--- test/Aggregators/LiquidityPoolAggregator.test.ts
+++ test/Aggregators/LiquidityPoolAggregator.test.ts
@@ -2,7 +2,6 @@ import { expect } from "chai";
 import sinon from "sinon";
 import {
   LiquidityPoolAggregator,
-  handlerContext,
 } from "../../generated/src/Types.gen";
 import {
   setLiquidityPoolAggregatorSnapshot,
@@ -26,49 +25,66 @@ describe("LiquidityPoolAggregator Functions", () => {
     timestamp = new Date();
   });
 
-  it("should create a snapshot of the liquidity pool aggregator", () => {
-    setLiquidityPoolAggregatorSnapshot(
-      liquidityPoolAggregator as LiquidityPoolAggregator,
-      timestamp,
-      contextStub
-    );
+  describe("Snapshot Creation", () => {
 
-    expect(contextStub.LiquidityPoolAggregatorSnapshot.set.calledOnce).to.be
-      .true;
-    const snapshot =
-      contextStub.LiquidityPoolAggregatorSnapshot.set.getCall(0).args[0];
-    expect(snapshot.id).to.equal(
-      `${liquidityPoolAggregator.chainId}-${
-        liquidityPoolAggregator.id
-      }_${timestamp.getTime()}`
-    );
-    expect(snapshot.pool).to.equal(liquidityPoolAggregator.id);
+    beforeEach(() => {
+      setLiquidityPoolAggregatorSnapshot(
+        liquidityPoolAggregator as LiquidityPoolAggregator,
+        timestamp,
+        contextStub
+      );
+    })
+
+    it("should create a snapshot of the liquidity pool aggregator", () => {
+
+      expect(contextStub.LiquidityPoolAggregatorSnapshot.set.calledOnce).to.be
+        .true;
+      const snapshot =
+        contextStub.LiquidityPoolAggregatorSnapshot.set.getCall(0).args[0];
+      expect(snapshot.id).to.equal(
+        `${liquidityPoolAggregator.chainId}-${
+          liquidityPoolAggregator.id
+        }_${timestamp.getTime()}`
+      );
+      expect(snapshot.pool).to.equal(liquidityPoolAggregator.id);
+    });
+
   });
 
-  it("should update the liquidity pool aggregator and create a snapshot if needed", () => {
-    const diff = {
-      totalVolume0: 5000n,
-      totalVolume1: 6000n,
-      totalVolumeUSD: 7000n,
-      numberOfSwaps: 11n,
-    };
+  describe("Updating the Liquidity Pool Aggregator", () => {
 
-    updateLiquidityPoolAggregator(
-      diff,
-      liquidityPoolAggregator as LiquidityPoolAggregator,
-      timestamp,
-      contextStub
-    );
+    let diff: any;
+    beforeEach(() => {
+      diff = {
+        totalVolume0: 5000n,
+        totalVolume1: 6000n,
+        totalVolumeUSD: 7000n,
+        numberOfSwaps: 11n,
+      };
+      updateLiquidityPoolAggregator(
+        diff,
+        liquidityPoolAggregator as LiquidityPoolAggregator,
+        timestamp,
+        contextStub
+      );
+    })
 
-    expect(contextStub.LiquidityPoolAggregator.set.calledTwice).to.be.true;
-    const updatedAggregator =
-      contextStub.LiquidityPoolAggregator.set.getCall(0).args[0];
-    expect(updatedAggregator.totalVolume0).to.equal(diff.totalVolume0);
-    expect(updatedAggregator.totalVolume1).to.equal(diff.totalVolume1);
-    expect(updatedAggregator.numberOfSwaps).to.equal(diff.numberOfSwaps);
+    it("should update the liquidity pool aggregator", () => {
 
-    const snapshot =
-      contextStub.LiquidityPoolAggregatorSnapshot.set.getCall(0).args[0];
-    expect(snapshot).to.not.be.undefined;
+      expect(contextStub.LiquidityPoolAggregator.set.calledOnce).to.be.true;
+      const updatedAggregator =
+        contextStub.LiquidityPoolAggregator.set.getCall(0).args[0];
+      expect(updatedAggregator.totalVolume0).to.equal(diff.totalVolume0);
+      expect(updatedAggregator.totalVolume1).to.equal(diff.totalVolume1);
+      expect(updatedAggregator.numberOfSwaps).to.equal(diff.numberOfSwaps);
+    });
+
+    it("should create a snapshot if the last update was more than 1 hour ago", () => {
+      const snapshot =
+        contextStub.LiquidityPoolAggregatorSnapshot.set.getCall(0).args[0];
+      expect(snapshot).to.not.be.undefined;
+    });
+
   });
+
 });
