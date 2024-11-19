7106815775118ba469f5f67dc7dfe5b2a31ae0a2
jrab227@gmail.com <jrab227@gmail.com>
2024-11-19 12:38:11 -0500

    Stores standard pool reserves as nominal token values, and cleans up tests.

--- test/EventHandlers/Pool/Fees.test.ts
+++ test/EventHandlers/Pool/Fees.test.ts
@@ -5,34 +5,37 @@ import { setupCommon } from "./common";
 import { TEN_TO_THE_18_BI, TEN_TO_THE_6_BI, toChecksumAddress, TokenIdByChain } from "../../../src/Constants";
 
 describe("Pool Fees Event", () => {
-  const { mockToken0Data, mockToken1Data } = setupCommon();
+  const { mockToken0Data, mockToken1Data, mockLiquidityPoolData } = setupCommon();
+  const poolId = mockLiquidityPoolData.id;
 
   let mockDb: any;
-  beforeEach(() => {
+  let updatedDB: any;
+  let expectations: any = {
+    amount0In: 3n * 10n ** 18n,
+    amount1In: 2n * 10n ** 6n,
+    totalLiquidityUSD: 0n,
+  };
+
+  expectations.totalLiquidityUSD =
+    (mockLiquidityPoolData.reserve0 - expectations.amount0In) * mockToken0Data.pricePerUSDNew / ( 10n ** (mockToken0Data.decimals) )  +
+    (mockLiquidityPoolData.reserve1 - expectations.amount1In) * mockToken1Data.pricePerUSDNew / ( 10n ** (mockToken1Data.decimals) );
+  
+  expectations.totalFeesUSD =
+        mockLiquidityPoolData.totalFeesUSD +
+          (expectations.amount0In / 10n ** (mockToken0Data.decimals) ) * mockToken0Data.pricePerUSDNew +
+          (expectations.amount1In / 10n ** (mockToken1Data.decimals) ) * mockToken1Data.pricePerUSDNew;
+
+  let updatedPool: any;
+  
+  beforeEach(async () => {
     mockDb = MockDb.createMockDb();
-  });
-
-  it("should update LiquidityPool entity with new fees", async () => {
-    const poolId = toChecksumAddress("0x3333333333333333333333333333333333333333");
-    const mockLiquidityPool: LiquidityPoolAggregator = {
-      id: poolId,
-      chainId: 10,
-      token0_id: TokenIdByChain("0x1111111111111111111111111111111111111111", 10),
-      token1_id: TokenIdByChain("0x2222222222222222222222222222222222222222", 10),
-      token0_address: "0x1111111111111111111111111111111111111111",
-      token1_address: "0x2222222222222222222222222222222222222222",
-      totalFees0: 0n,
-      totalFees1: 0n,
-      totalFeesUSD: 0n,
-    } as LiquidityPoolAggregator;
-
-    const updatedDB1 = mockDb.entities.Token.set(mockToken0Data as Token);
-    const updatedDB2 = updatedDB1.entities.Token.set(mockToken1Data as Token);
-    const updatedDB3 = updatedDB2.entities.LiquidityPoolAggregator.set(mockLiquidityPool);
+    updatedDB = mockDb.entities.Token.set(mockToken0Data as Token);
+    updatedDB = updatedDB.entities.Token.set(mockToken1Data as Token);
+    updatedDB = updatedDB.entities.LiquidityPoolAggregator.set(mockLiquidityPoolData);
 
     const mockEvent = Pool.Fees.createMockEvent({
-      amount0: 100n * TEN_TO_THE_18_BI,
-      amount1: 200n * TEN_TO_THE_6_BI,
+      amount0: expectations.amount0In,
+      amount1: expectations.amount1In,
       mockEventData: {
         block: {
           number: 123456,
@@ -40,20 +43,33 @@ describe("Pool Fees Event", () => {
           hash: "0x1234567890123456789012345678901234567890123456789012345678901234",
         },
         chainId: 10,
-        srcAddress: toChecksumAddress("0x3333333333333333333333333333333333333333"),
+        srcAddress: poolId,
       },
     });
 
     const result = await Pool.Fees.processEvent({
       event: mockEvent,
-      mockDb: updatedDB3,
+      mockDb: updatedDB,
     });
 
-    const updatedPool = result.entities.LiquidityPoolAggregator.get(poolId);
+    updatedPool = result.entities.LiquidityPoolAggregator.get(poolId);
+  });
+
+  it("should update LiquidityPoolAggregator", async () => {
     expect(updatedPool).to.not.be.undefined;
-    expect(updatedPool?.totalFees0).to.equal(100n * TEN_TO_THE_18_BI);
-    expect(updatedPool?.totalFees1).to.equal(200n * TEN_TO_THE_18_BI);
-    expect(updatedPool?.totalFeesUSD).to.equal(300n * TEN_TO_THE_18_BI); // 100 + 200
-    expect(updatedPool?.lastUpdatedTimestamp).to.deep.equal(new Date(1000000 * 1000));
+    expect(updatedPool.lastUpdatedTimestamp).to.deep.equal(new Date(1000000 * 1000));
+  });
+
+  it("should update LiquidityPoolAggregator nominal fees", async () => {
+    expect(updatedPool.totalFees0).to.equal(
+      mockLiquidityPoolData.totalFees0 + expectations.amount0In
+    );
+    expect(updatedPool.totalFees1).to.equal(
+      mockLiquidityPoolData.totalFees1 + expectations.amount1In
+    );
+  });
+
+  it("should update LiquidityPoolAggregator total fees in USD", async () => {
+    expect(updatedPool.totalFeesUSD).to.equal(expectations.totalFeesUSD);
   });
 });
