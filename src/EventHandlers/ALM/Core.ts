2e901919aa3986cf6193fc1acc4fc2165ccb7c1b
jrab227@gmail.com <jrab227@gmail.com>
2025-01-17 07:59:04 -0500

    Add tracking for AMM positions.

--- src/EventHandlers/ALM/Core.ts
+++ src/EventHandlers/ALM/Core.ts
@@ -1,11 +1,12 @@
 import {
     ALMCore,
     ALMCore_Rebalance,
+    ALMCore_Rebalance_AmmPosition,
 } from "generated";
 
 ALMCore.Rebalance.handler(async ({ event, context }) => {
   const [ pool, ammPositionInfo, sqrtPriceX96, amount0, amount1, ammPositionIdBefore, ammPositionIdAfter ] = event.params.rebalanceEventParams;
-  const [ token0, token1, property, tickLower, tickUpper ] = ammPositionInfo;
+  const [ token0, token1, property, tickLower, tickUpper, liquidity ] = ammPositionInfo;
   const entity: ALMCore_Rebalance = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     pool,
@@ -23,4 +24,21 @@ ALMCore.Rebalance.handler(async ({ event, context }) => {
   };
 
   context.ALMCore_Rebalance.set(entity);
+
+  const ammPosition_entity: ALMCore_Rebalance_AmmPosition = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    pool,
+    token0,
+    token1,
+    property,
+    tickLower,
+    tickUpper,
+    liquidity,
+    timestamp: new Date(event.block.timestamp * 1000),
+    chainId: event.chainId,
+    blockNumber: event.block.number,
+    logIndex: event.logIndex
+  };
+
+  context.ALMCore_Rebalance_AmmPosition.set(ammPosition_entity);
 }); 
--- src/EventHandlers/ALM/DeployFactory.ts
+++ src/EventHandlers/ALM/DeployFactory.ts
@@ -1,4 +1,4 @@
-import { ALMDeployFactory, ALMDeployFactory_StrategyCreated } from "generated";
+import { ALMDeployFactory, ALMDeployFactory_StrategyCreated, ALMDeployFactory_StrategyCreated_AmmPosition } from "generated";
 
 
 ALMDeployFactory.StrategyCreated.contractRegister(
@@ -24,15 +24,61 @@ ALMDeployFactory.StrategyCreated.handler(async ({ event, context }) => {
     caller
   ] = event.params.params;
 
-  const entity: ALMDeployFactory_StrategyCreated = {
+  const [
+    strategyType,
+    tickNeighborhood,
+    tickSpacing,
+    width,
+    maxLiquidityRatioDeviationX96
+  ] = strategyParams;
+
+  const ammPositionList = ammPosition.map((vals) => {
+    const [token0, token1, property, tickLower, tickUpper, liquidity] = vals;
+    return {
+      token0,
+      token1,
+      property,
+      tickLower,
+      tickUpper,
+      liquidity
+    };
+  });
+
+  const strategy_created_entity: ALMDeployFactory_StrategyCreated = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     pool,
     lpWrapper,
+    strategyType,
+    tickNeighborhood,
+    tickSpacing,
+    width,
+    maxLiquidityRatioDeviationX96,
     timestamp: new Date(event.block.timestamp * 1000),
     chainId: event.chainId,
     blockNumber: event.block.number,
     logIndex: event.logIndex
   };
 
-  context.ALMDeployFactory_StrategyCreated.set(entity);
+  context.ALMDeployFactory_StrategyCreated.set(strategy_created_entity);
+
+  for (const ammPosition of ammPositionList) {
+    const { token0, token1, property, tickLower, tickUpper, liquidity } = ammPosition;
+    const ammPosition_entity: ALMDeployFactory_StrategyCreated_AmmPosition = {
+      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+      pool,
+      lpWrapper,
+      token0,
+      token1,
+      property,
+      tickLower,
+      tickUpper,
+      liquidity,
+      timestamp: new Date(event.block.timestamp * 1000),
+      chainId: event.chainId,
+      blockNumber: event.block.number,
+      logIndex: event.logIndex
+    };
+
+    context.ALMDeployFactory_StrategyCreated_AmmPosition.set(ammPosition_entity);
+  }
 });
